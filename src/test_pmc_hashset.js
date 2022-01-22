d6936756366a0b311c8c3c65bab8acac36ed1fc8
Ciprian Teodorov <ciprian.teodorov@gmail.com>
2022-01-22 23:47:15 +0100

    added examples with abstraction

--- src/test_pmc_hashset.js
+++ src/test_pmc_hashset.js
@@ -77,7 +77,6 @@ console.log("deadlock " + JSON.stringify(result));
      initial: model.initial,
      next: (c) => model.ss[c],
  };
- 
 
 //exclusion
 result = hashset_predicate_mc(tr, model.exclusion, (c,s) => c, 0, (a, b) => a === b, Number.MAX_SAFE_INTEGER, (c) => c);
@@ -87,3 +86,27 @@ console.log("exclusion " + JSON.stringify(result));
 deadlockPred = (c) => tr.next(c).length == 0
 result = hashset_predicate_mc(tr, deadlockPred, (c,s) => c, 0, (a, b) => a === b, Number.MAX_SAFE_INTEGER, (c) => c);
 console.log("deadlock " + JSON.stringify(result));
+
+/**
+ * bitstate hashing without a bloom-filter, looses a lot of space
+ */
+deadlockPred = (c) => tr.next(c).length == 0;
+let hash = (c,s) => c%8;
+let bloom_size = 3;
+let abstraction = (c) => hash(c) % bloom_size;
+result = hashset_predicate_mc(tr, deadlockPred, (c,s) => c, 0, (a, b) => a === b, Number.MAX_SAFE_INTEGER, abstraction);
+console.log("bitstate deadlock " + JSON.stringify(result));
+
+/**
+ * hashcompaction, works perfectly with our implementation.
+ */
+abstraction = (c) => hash(c);
+result = hashset_predicate_mc(tr, deadlockPred, (c,s) => c, 0, (a, b) => a === b, Number.MAX_SAFE_INTEGER, abstraction);
+console.log("hashcompaction deadlock " + JSON.stringify(result));
+
+/**
+ * Predicate abstraction
+ */
+abstraction = (c) => [model.aCS, model.bCS];
+result = hashset_predicate_mc(tr, deadlockPred, (c,s) => c[0]+c[1], 0, (a, b) => a[0] === b[0] && a[1]===b[1], Number.MAX_SAFE_INTEGER, abstraction);
+console.log("predicate under-approximation deadlock " + JSON.stringify(result));
