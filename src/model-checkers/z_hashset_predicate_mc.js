e7dd8cb312e493fa646d7f5bd962f3e2deff8314
Ciprian Teodorov <ciprian.teodorov@gmail.com>
2022-01-22 23:35:42 +0100

    added some placeholdes to indicate how we could implement multiple storage backends.

--- src/model-checkers/z_hashset_predicate_mc.js
+++ src/model-checkers/z_hashset_predicate_mc.js
@@ -12,3 +12,35 @@ function hashset_predicate_mc(tr, acceptingPredicate, hashFn, hashSeed, equality
 
     return dataless_predicate_mc(tr, acceptingPredicate, known, frontier, parentTree, bound, canonize);
 }
+
+//TODO: implement a real bloom-filter
+function bloom_predicate_mc(tr, acceptingPredicate, hashFn, hashSeed, bound, canonize) {
+
+    let known       = new BloomFilter(1024);
+    let frontier    = new PingPongCircularBuffer(1024);
+    let parentTree  = new LinearScanHashSet(1024, hashFn, hashSeed, equalityFn, true); 
+
+    return dataless_predicate_mc(tr, acceptingPredicate, known, frontier, parentTree, bound, canonize);
+}
+
+//TODO: implement a real BDD
+// marshaller should be something that transforms a configuration in a boolean array (maybe the SLI marshaller and we handle it here ?)
+function bdd_predicate_mc(tr, acceptingPredicate, marshaller, bound, canonize) {
+
+    let known       = new BDD(1024);
+    let frontier    = new PingPongCircularBuffer(1024);
+    let parentTree  = new LinearScanHashSet(1024, hashFn, hashSeed, equalityFn, true); 
+    let bdd_canonize = (c) => project(canonize(c))
+    return dataless_predicate_mc(tr, acceptingPredicate, known, frontier, parentTree, bound, bdd_canonize);
+}
+
+//TODO: implement a real minimal DFA
+// marshaller should be something that can iterate over the configuration contents to produce an array of letters for the DFA.
+function dfa_predicate_mc(tr, acceptingPredicate, configuration_iterator, bound, canonize) {
+
+    let known       = new MinimalDFA(1024);
+    let frontier    = new PingPongCircularBuffer(1024);
+    let parentTree  = new LinearScanHashSet(1024, hashFn, hashSeed, equalityFn, true); 
+    let dfa_canonize = (c) => configuration_iterator(canonize(c))
+    return dataless_predicate_mc(tr, acceptingPredicate, known, frontier, parentTree, bound, canonize);
+}
