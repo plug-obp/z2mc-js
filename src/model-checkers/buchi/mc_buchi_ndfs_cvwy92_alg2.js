d7d024deebbd15b6522a183835f51fa2a989fbcc
Ciprian Teodorov <ciprian.teodorov@gmail.com>
2022-02-19 23:09:25 +0100

    fixed and tested the cvwy92 algorithm

--- src/model-checkers/buchi/mc_buchi_ndfs_cvwy92_alg2.js
+++ src/model-checkers/buchi/mc_buchi_ndfs_cvwy92_alg2.js
@@ -20,10 +20,15 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-import { dataless_dfs_traversal } from "../algorithms/z_dataless_dfs.js";
+import { dataless_dfs_traversal } from "../../algorithms/z_dataless_dfs.js";
+import { LinearScanHashSet      } from "../../datastructures/linear_scan_set.js"
+import { UnboundedStack         } from "../../datastructures/unbounded_stack.js";
+
+export {ndfs_cvwy92_alg2}
 
 /**
- * CVWY92_Algorithm2 is the algorithm 2 from [1]. The recursive pseudocode seems to be:
+ * CVWY92_Algorithm2 is the algorithm 2 from [1]. 
+ * The recursive pseudocode seems to be:
 ```
 dfs₁(s, k₁ = ∅, k₂ = ∅)
     k₁ = k₁ ∪ { s }
@@ -52,69 +57,71 @@ dfs₂(s, seed, k₂)
  * Formal methods in system design 1, no. 2 (1992): 275-288.
  */
 
-function CVWY92_Algorithm2(initial, next, canonize, acceptingPredicate, hashFn, equalityFn) {
+function ndfs_cvwy92_alg2(initial, next, canonize, acceptingPredicate, hashFn, equalityFn) {
     let known1      = new LinearScanHashSet(1024, hashFn, equalityFn, false);
     let stack1      = new UnboundedStack(1024, 2);
     let known2      = new LinearScanHashSet(1024, hashFn, equalityFn, false);
     let stack2      = new UnboundedStack(1024, 2);
-    return CVWY92_Algorithm2_dfs1(initial, next, canonize, acceptingPredicate, known1, stack1, known2, stack2);
+    return dfs1(initial, next, canonize, acceptingPredicate, known1, stack1, known2, stack2);
 }
 
 //the first DFS checks the accepting predicate in postorder (on_exit)
-function CVWY92_Algorithm2_dfs1(initial, next, canonize, acceptingPredicate, known1, stack1, known2, stack2) {
+function dfs1(initial, next, canonize, acceptingPredicate, known1, stack1, known2, stack2) {
     function addIfAbsent(n, nc) {
         return known1.add(nc);
     }
+    function on_entry(s,n,nc,m) {
+        m.cc++;
+    }
     function on_exit(n, frame, mem) {
         if (acceptingPredicate(n)) {
-            let {holds, _, cc, trace} = CVWY92_Algorithm2_dfs2(n, next, canonize, known2, stack2);
-            mem.holds = holds;
-            mem.witness = mem.holds ? n : null;
-            mem.cc += cc;
-            mem.trace = mem.holds ? stack1.map(e => e.configuration).slice(1).push(n) + trace : [];
-            return mem.holds;
+            let result = dfs2(n, next, canonize, known2, stack2);
+            if (result.holds) {
+                return false;
+            }
+            mem.holds = false;
+            mem.witness = n;
+            mem.trace = stack1.map(e => e.configuration).slice(1);
+            mem.trace.push(...result.trace);
+            return true;
         }
-        mem.cc++;
         return false;
     };
     let memory = {
         holds:   true,
         witness: null,
-        cc: 0, 
         trace: [],
+        cc: 0, 
     };
-    let {holds, witness, configuration_count, trace} = dataless_dfs_traversal(
+    let {holds, witness, trace, cc} = dataless_dfs_traversal(
         initial, next, canonize,
-        (s,n,cn,m)=>false, (s,n,cn,m) => false, on_exit, memory, 
+        on_entry, (s,n,cn,m) => false, on_exit, memory, 
         addIfAbsent, stack1);
-    return {verified: !holds, trace: trace, configuration_count};
+    return {verified: !holds, trace: trace, configuration_count: cc};
 }
 
 //the second DFS checks the accepting predicate in preorder (on_entry)
-function CVWY92_Algorithm2_dfs2(seed, next, canonize, known, stack) {
+function dfs2(seed, next, canonize, known, stack) {
     function addIfAbsent(n, nc) {
         return known.add(nc);
     }
     function on_entry(s,n,cn,mem) {
         //if seed ∈ next(s) then report violation
         if (next(n).find((e) => e === seed)) {
-            mem.holds = true;
+            mem.holds = false;
             mem.witness = seed;
-            mem.cc += 1;
             mem.trace = stack.map(e => e.configuration).slice(1);
             return true;
         }
-        mem.cc++;
         return false;
     };
     let memory = {
         holds:   true,
         witness: null,
-        cc: 0, 
         trace: [],
     };     
     return dataless_dfs_traversal(
-        initial, next, canonize,
+        [seed], next, canonize,
         on_entry, (s,n,cn,m) => false, (n,frame,m)=>false, memory, 
         addIfAbsent, stack);
 }
