b3b999e5e44951204e44cb262b1cba8a17b5e91c
Matthias PASQUIER <matthias.pasquier@ertosgener.com>
2023-01-12 19:37:45 +0100

    Replaced kripke and buchi to rhs and lhs

--- src/operators/str/synchronous_product_semantics.js
+++ src/operators/str/synchronous_product_semantics.js
@@ -39,11 +39,11 @@ class StateSynchronousProductSemantics {
     configurationEqFn(x, y) {
         let { lc:xlc, rc:xrc } = x;
         let { lc:ylc, rc:yrc } = y;
-        return this.lhs.configurationEqFn(xlc, ylc) && this.buchi.configurationEqFn(xrc, yrc);
+        return this.lhs.configurationEqFn(xlc, ylc) && this.rhs.configurationEqFn(xrc, yrc);
     };
 
     async initial() {
-        return await this.rhs.initial().map((abc) => ({ lc: null, rc: abc }));
+        return await this.rhs.initial().map((arc) => ({ lc: null, rc: arc }));
     }
 
     async actions(source) {
@@ -52,7 +52,7 @@ class StateSynchronousProductSemantics {
         //initial case: the kripke configuration is None -- the synthetic configuration introduced by the kripke2buchi
         if (l_source == null) {
             for (let l_target of await this.lhs.initial()) {
-                await getSynchronousActions(this.buchi, l_target, r_source, synchronous_actions);
+                await getSynchronousActions(this.rhs, l_target, r_source, synchronous_actions);
             }
             return synchronous_actions;
         }
@@ -66,7 +66,7 @@ class StateSynchronousProductSemantics {
                 continue;
             }
             for (let l_target of l_targets) {
-                await getSynchronousActions(this.buchi, l_target, r_source, synchronous_actions);
+                await getSynchronousActions(this.rhs, l_target, r_source, synchronous_actions);
             }
         }
         //the deadlock case: the kripke does not have a step, add stuttering
@@ -80,7 +80,7 @@ class StateSynchronousProductSemantics {
         let { lo: l_target, ra: r_action } = action;
         let { lc: _, rc: r_source } = configuration;
         let r_targets = await this.rhs.execute(r_action, l_target, r_source);
-        return r_targets.map((abc) => ({ lc: kripke_target, rc: abc }));
+        return r_targets.map((arc) => ({ lc: l_target, rc: arc }));
     }
 
     async isAccepting(configuration) {
@@ -97,71 +97,71 @@ async function getSynchronousActions(r_semantics, l_output, r_config, io_synchro
 }
 const STUTTERING = {};
 class StepSynchronousProductSemantics {
-    constructor(kripke, buchi) {
-        this.kripke = kripke;
-        this.buchi = buchi;
+    constructor(lhs, rhs) {
+        this.lhs = lhs;
+        this.rhs = rhs;
     }
 
     configurationHashFn (configuration) {
-        let { kc, bc } = configuration;
-        let seed = this.kripke.configurationHashFn(kc);
-        let value = this.buchi.configurationHashFn(bc);
+        let { lc, rc } = configuration;
+        let seed = this.lhs.configurationHashFn(lc);
+        let value = this.rhs.configurationHashFn(rc);
         seed ^= value + 0x9e3779b9 + (seed << 6) + (seed >> 2);
         return seed;
     }
 
     configurationEqFn(x, y) {
-        let { kc:xkc,bc: xbc } = x;
-        let { kc:ykc, bc:ybc } = y;
-        return this.kripke.configurationEqFn(xkc, ykc) && this.buchi.configurationEqFn(xbc, ybc);
+        let { lc:xlc,rc: xrc } = x;
+        let { lc:ylc, rc:yrc } = y;
+        return this.lhs.configurationEqFn(xlc, ylc) && this.rhs.configurationEqFn(xrc, yrc);
     };
 
     async initial() {
         let initial_configurations = [];
-        for (let kripke_config of await this.kripke.initial()) {
-            for (let buchi_config of await this.buchi.initial()) {
-                initial_configurations.push({ kc: kripke_config, bc: buchi_config });
+        for (let l_config of await this.lhs.initial()) {
+            for (let r_config of await this.rhs.initial()) {
+                initial_configurations.push({ lc: l_config, rc: r_config });
             }
         }
         return initial_configurations;
     }
 
     async actions(source) {
-        let { kc: kripke_source, bc: buchi_source } = source;
+        let { lc: l_source, rc: r_source } = source;
         let synchronous_actions = [];
         //the normal case: the kripke has a step, find the corresponding step in the buchi automaton
-        let kripke_actions = await this.kripke.actions(kripke_source);
-        let number_of_actions = kripke_actions.length; //used to detect deadlock
-        for (let kripke_action of kripke_actions) {
-            let kripke_targets = await this.kripke.execute(kripke_action, kripke_source);
-            if (kripke_targets.length == 0) {
+        let l_actions = await this.lhs.actions(l_source);
+        let number_of_actions = l_actions.length; //used to detect deadlock
+        for (let l_action of l_actions) {
+            let l_targets = await this.lhs.execute(l_action, l_source);
+            if (l_targets.length == 0) {
                 number_of_actions -= 1;
                 continue;
             }
-            for (let kripke_target of kripke_targets) {
-                let kripke_step = { s: kripke_source, a: kripke_action, t: kripke_target };
-                await getSynchronousActions(this.buchi, kripke_step, buchi_source, synchronous_actions);
+            for (let l_target of l_targets) {
+                let l_step = { s: l_source, a: l_action, t: l_target };
+                await getSynchronousActions(this.rhs, l_step, r_source, synchronous_actions);
             }
         }
         //the deadlock case: the kripke does not have a step, add stuttering
         if (number_of_actions == 0) {
-            let kripke_step = { s: kripke_source, a: STUTTERING, t: kripke_source };
-            await getSynchronousActions(this.buchi, kripke_step, buchi_source, synchronous_actions);
+            let l_step = { s: l_source, a: STUTTERING, t: l_source };
+            await getSynchronousActions(this.rhs, l_step, r_source, synchronous_actions);
         }
         return synchronous_actions;
     }
 
     async execute(action, configuration) {
-        let { ko: kripke_step, ba: buchi_action } = action;
-        let { kc: kc, bc: buchi_source } = configuration;
-        let { s: s, a: a, t: kripke_target } = kripke_step;
-        let buchi_targets = await this.buchi.execute(buchi_action, kripke_step, buchi_source);
-        return buchi_targets.map((abc) => ({ kc: kripke_target, bc: abc }));
+        let { lo: l_step, ra: r_action } = action;
+        let { lc: lc, rc: r_source } = configuration;
+        let { s: s, a: a, t: l_target } = l_step;
+        let r_targets = await this.rhs.execute(r_action, l_step, r_source);
+        return r_targets.map((arc) => ({ lc: l_target, rc: arc }));
     }
 
     async isAccepting(configuration) {
-        let { kc, bc } = configuration;
-        return await this.kripke.isAccepting(kc) && await this.buchi.isAccepting(bc);
+        let { lc, rc } = configuration;
+        return await this.lhs.isAccepting(lc) && await this.rhs.isAccepting(rc);
     }
 }
 
