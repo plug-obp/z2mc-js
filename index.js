49a1350fcd6baf26282032948e639525c51c6df5
Ciprian Teodorov <ciprian.teodorov@gmail.com>
2024-02-07 14:38:48 +0100

    export StateSynchronousProductSemantics

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { STR2TR } from "./src/operators/str/str2tr.js";
-import { StepSynchronousProductSemantics } from "./src/operators/str/synchronous_product_semantics.js";
+import { StateSynchronousProductSemantics, StepSynchronousProductSemantics } from "./src/operators/str/synchronous_product_semantics.js";
 import {ndfs_gs09_cdlp05} from "./src/model-checkers/buchi/mc_buchi_ndfs_gs09_cdlp05.js"
 
-export { STR2TR, StepSynchronousProductSemantics, ndfs_gs09_cdlp05 }
+export { STR2TR, StateSynchronousProductSemantics, StepSynchronousProductSemantics, ndfs_gs09_cdlp05 }
