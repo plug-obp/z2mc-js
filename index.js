604d80cb1808941c206b76445b959d1259a28946
Ciprian Teodorov <ciprian.teodorov@gmail.com>
2024-05-22 18:54:25 +0200

    export dfs predicate mc

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { STR2TR } from "./src/operators/str/str2tr.js";
 import { StateSynchronousProductSemantics, StepSynchronousProductSemantics } from "./src/operators/str/synchronous_product_semantics.js";
 import {ndfs_gs09_cdlp05} from "./src/model-checkers/buchi/mc_buchi_ndfs_gs09_cdlp05.js"
+import {dfs_hashset_predicate_mc_full} from "./src/model-checkers/z_hashset_predicate_mc.js"
 
-export { STR2TR, StateSynchronousProductSemantics, StepSynchronousProductSemantics, ndfs_gs09_cdlp05 }
+export { STR2TR, StateSynchronousProductSemantics, StepSynchronousProductSemantics, ndfs_gs09_cdlp05, dfs_hashset_predicate_mc_full }
